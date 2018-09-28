f1dbcd7a7bb479b037e5731ff8c7414586dba763
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-09-27 14:38:16 -1000

    all tests pass

--- js/nested-for-loops.js
+++ js/nested-for-loops.js
@@ -4,11 +4,24 @@
  * @param {number} [width=depth] 
  * @returns {string}
  */
-function nestedForLoops (depth, width = depth) {
+function nestedForLoops(depth, width = depth) {
+  if (isNaN(depth) || isNaN(width)) {
+    return "";
+  }
   let result = "\n";
 
-  // Write code here
-
+  for (let i = 0; i < depth; i++) {
+    for (let j = 0; j < width; j++) {
+      result += `{x:${j}, y:${i}}`
+      if (j < width-1) {
+        result += `, `
+      }
+      if (j === width - 1) {
+        result += "\n"
+      }
+    }
+  }
+  return result;
 };
 
 // To see your console output outside the tests add function calls here.
